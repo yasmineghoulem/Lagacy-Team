4ced211b2213549f38a993cfbe612b19ce1035f2
Yasmine <yasmine.ghouleme@gmail.com>
2020-12-18 10:40:06 +0100

    login register done!

---
+++ Front/src/app/auth.guard.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { Router, CanActivate } from '@angular/router';
+import { UserService } from './services/user.service';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  constructor(private router: Router, private userService: UserService) {}
+
+  canActivate() {
+    if (this.userService.loggedIn()) {
+      return true;
+    } else {
+      this.router.navigate(['/login']);
+      return false;
+    }
+  }
+}
